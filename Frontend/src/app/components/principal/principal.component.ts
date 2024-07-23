d807e58e42abd44b0cfa1e6e4b01ebec70ba3349
Alberto-Zapardiel-Fernandez <alberto.zapardiel.fernandez@gmail.com>
2024-07-23 21:17:58 +0200

    Agregado componente card

--- Frontend/src/app/components/principal/principal.component.ts
+++ Frontend/src/app/components/principal/principal.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
+import { CardComponent } from '../card/card.component';
 
 @Component({
   selector: 'app-principal',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './principal.component.html',
   styleUrl: './principal.component.css',
 })
 export class PrincipalComponent implements OnInit {
   constructor(private cookieService: CookieService) {}
+
+  prueba: number = 7;
+  textoCard1: string = 'Hola1';
+  textoCard2: string = 'Hola2';
   user: any;
   ngOnInit(): void {
     this.user = JSON.parse(this.cookieService.get('user'));
