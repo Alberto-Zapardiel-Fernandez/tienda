482e4902cd44f170a8e9b6bae226061b7e3067a7
Alberto-Zapardiel-Fernandez <alberto.zapardiel.fernandez@gmail.com>
2024-07-24 20:45:01 +0200

    Refcator para quitar la card

--- Frontend/src/app/components/principal/principal.component.ts
+++ Frontend/src/app/components/principal/principal.component.ts
@@ -1,20 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
-import { CardComponent } from '../card/card.component';
 
 @Component({
   selector: 'app-principal',
   standalone: true,
-  imports: [CardComponent],
+  imports: [],
   templateUrl: './principal.component.html',
   styleUrl: './principal.component.css',
 })
 export class PrincipalComponent implements OnInit {
   constructor(private cookieService: CookieService) {}
 
-  prueba: number = 7;
-  textoCard1: string = 'Hola1';
-  textoCard2: string = 'Hola2';
   user: any;
   ngOnInit(): void {
     this.user = JSON.parse(this.cookieService.get('user'));
