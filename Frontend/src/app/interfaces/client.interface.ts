0b705f3e12519069323c46431120efd99fb3c3d9
Alberto-Zapardiel-Fernandez <alberto.zapardiel.fernandez@gmail.com>
2024-08-01 20:29:00 +0200

    Creada la interfaz de cliente y las redirecciones

---
+++ Frontend/src/app/interfaces/client.interface.ts
@@ -0,0 +1,10 @@
+export interface ClientInterface {
+  id: number;
+  name: string;
+  lastName: string;
+  email: string;
+  dni: string;
+  phone: string;
+  address: string;
+  discount: number;
+}
